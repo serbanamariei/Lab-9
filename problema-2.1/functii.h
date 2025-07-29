558e0e55466eb879f32f5fe060ea938300b62305
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 19:54:11 +0300

    Create functii.h

---
+++ problema-2.1/functii.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+double **citire(int n);
+void afisare(double **a, int n);
+double normaInfinit(double **a, int n);
+double norma1(double **a, int n);
+double norma2(double **a, int n);
+
+#endif
