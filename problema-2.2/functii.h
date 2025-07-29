687884d92953753b75076edff425ab9101973bd8
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 19:54:56 +0300

    Create functii.h

---
+++ problema-2.2/functii.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+double **citire(int n);
+void afisare(double **a, int n);
+double normaInfinit(double **a, int n);
+double norma1(double **a, int n);
+double norma2(double **a, int n);
+double suma(double **a,int n);
+double produs(double **a, int n);
+double mediaAritmetica(double **a, int n);
+double mediaGeo(double **a, int n);
+void oglindaSusJos(double **a, int n);
+void oglindaStDrt(double **a, int n);
+
+#endif
