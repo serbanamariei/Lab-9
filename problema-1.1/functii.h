d5356f50ffcdc86ca3fdef46f65a8ba5b4ba496f
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 19:28:17 +0300

    Create functii.h

---
+++ problema-1.1/functii.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+int **citire(int n, int m);
+void dealocare(int **a, int n, int m);
+void afisare(int **a, int n, int m);
+void ordonare(int **a, int n, int m);
+int maxim(int *a, int m);
+void interschimbare(int **a, int **b);
+
+#endif
